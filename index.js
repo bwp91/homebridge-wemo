e8b23b84e1a209767e409500710c449b09bc2d50
Tim <TG908@users.noreply.github.com>
2016-02-27 00:13:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,41 @@
 var Service, Characteristic;
 var net = require('net');
+var HOST = '192.168.178.20';
+var PORT = 23;
 
-module.exports = function(homebridge){
+module.exports = function(homebridge) {
+
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-vsx", "VSX", VSXAccessory);
+  homebridge.registerAccessory("homebridge-vsx", "VSX", Computer);
 }
 
-function VSXAccessory(config) {
-  this.name = config["name"];
-  this.ip = config["ip"];
-  this.port = 23;
+function VSX(log, config) {
+  this.log = log;
+  this.name = config.name;
+  this.ip = config.ip;
+
+  this._service = new Service.Switch(this.name);
+  this._service.getCharacteristic(Characteristic.On)
+    .on('set', this._setOn.bind(this));
 }
 
-VSXAccessory.prototype.getPowerOn = function(callback) {
-
-console.log("wip");
-
-
+Computer.prototype.getServices = function() {
+  return [this._service];
 }
 
-VSXAccessory.prototype.setPowerOn = function(powerOn, callback) {
+Computer.prototype._setOn = function(on, callback) {
 
-  var client = new net.Socket();
-  
-client.connect(23, "192.168.178.20", function() {
+  if(on){
+    var client = new net.Socket();
+    client.connect(PORT, HOST, function() {
 
-    console.log('CONNECTED TO: ' + host + ':' + port);
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
     // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
-    //if powerOn == true {
-      client.write("PO");
-  //  } else if powerOn == false {
-      client.write("PF");
-  //  }
+    client.write('PO');
 
 });
-
-// Add a 'data' event handler for the client socket
-// data is what the server sent to this socket
-client.on('data', function(data) {
-    
-    console.log('DATA: ' + data);
-    // Close the client socket completely
-    client.destroy();
-    
-});
-
-// Add a 'close' event handler for the client socket
-client.on('close', function() {
-    console.log('Connection closed');
-});
-
-}
-
-VSXAccessory.prototype.getServices = function() {
-  
-    var switchService = new Service.Switch(this.name);
-    
-    switchService
-      .getCharacteristic(Characteristic.On)
-      .on('get', this.getPowerOn.bind(this))
-      .on('set', this.setPowerOn.bind(this));
-    
-    return [switchService];
- 
   }
+
+  callback();
+}
