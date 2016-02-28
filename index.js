020c6d444703c7c340174282b45f96b2eb51ea31
Tim <TG908@users.noreply.github.com>
2016-02-28 12:53:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,67 @@ module.exports = function(homebridge) {
 function VSX(log, config) {
   this.log = log;
   this.name = config.name;
-  this.ip = config.ip;
+  HOST = config.ip;
 
-  this._service = new Service.Switch(this.name);
-  this._service.getCharacteristic(Characteristic.On)
-    .on('set', this._setOn.bind(this));
+  this.service = new Service.Switch(this.name);
+  this.service.getCharacteristic(Characteristic.On)
+    .on("set", this.setOn.bind(this))
+    .on("get", this.getOn.bind(this));
 }
 
 VSX.prototype.getServices = function() {
-  return [this._service];
+  return [this.service];
 }
 
-VSX.prototype._setOn = function(on, callback) {
+VSX.prototype.getOn = function(callback) {
+  
+  var client = new net.Socket();
+  client.connect(PORT, HOST, function() {
+   
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    client.write('?P\r\n');
+
+  }); 
+    
+    client.on('data', function(data) {
+    
+      console.log('DATA: ' + data);
+      var str = data.toString();
+      
+      if (str.includes("PWR1")) {
+        console.log("AUS");
+        var on = false;
+        client.destroy();
+        callback(null,on);
+        
+      } else if (str.includes("PWR0")) {
+        console.log("AN");
+        var on = true;
+        client.destroy();
+        callback(null,on);
+        
+      } else {
+        console.log("waiting");
+      }
+
+  });
+  
+    client.on('close', function() {
+    console.log('Connection closed');
+    
+  });
+
+    client.on('error', function(ex) {
+      console.log("handled error");
+      console.log(ex);
+      callback(ex)
+    
+  }); 
+}
+
+
+
+VSX.prototype.setOn = function(on, callback) {
 
   if(on){
     var client = new net.Socket();
@@ -41,6 +90,11 @@ VSX.prototype._setOn = function(on, callback) {
     client.on('close', function() {
     console.log('Connection closed');
 
+});
+
+    client.on('close', function() {
+    console.log('Connection closed');
+    
 });
  
     client.on('error', function(ex) {
